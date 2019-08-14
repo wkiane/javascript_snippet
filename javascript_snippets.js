1db971abd392c9d04203053c762c5f3d00f0fdf6
willa kiane <willa_abreu@outlook.com>
2019-08-14 11:51:39 -0300

    is in view port

--- javascript_snippets.js
+++ javascript_snippets.js
@@ -1,3 +1,14 @@
 Array.prototype.sample = function(){
   return this[Math.floor(Math.random()*this.length)];
 }
+
+// if ($('#Something').isInViewport()) {}
+$.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+};
